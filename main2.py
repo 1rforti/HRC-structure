56d9176923dd36a87680b9a1a762d044e2c09348
1rforti <ricardoforti@hotmail.com>
2025-02-15 03:51:42 -0300

    uu

--- main2.py
+++ main2.py
@@ -1,4 +1,79 @@
-﻿@app.route('/process_images', methods=['POST'])
+﻿# -*- coding: utf-8 -*-
+
+import cv2
+import pytesseract
+from PIL import Image
+import os
+import numpy as np
+import re
+import json
+import sys
+from flask import Flask, request, jsonify, send_file, render_template
+
+app = Flask(__name__)  # Definição da instância do Flask
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/generate_json', methods=['POST'])
+def generate_json():
+    xml_file = request.files.get('xml_file')
+    stack_inicial = request.form.get('stack_inicial')
+
+    if not stack_inicial:
+        return jsonify({"error": "Por favor, informe o stack inicial."}), 400
+
+    if not xml_file:
+        return jsonify({"error": "Por favor, selecione um arquivo XML."}), 400
+
+    data_dict = read_xml(xml_file)
+    tournament_info = data_dict['CompletedTournament']
+    tournament_name = tournament_info['@name']
+    total_entrants = int(tournament_info['@totalEntrants']) + int(tournament_info['@reEntries'])
+    flags = tournament_info['@flags']
+
+    chips = total_entrants * int(stack_inicial)
+
+    output_data = {
+        "name": "/",
+        "folders": [],
+        "structures": [
+            {
+                "name": tournament_name,
+                "chips": chips,
+                "prizes": {}
+            }
+        ]
+    }
+
+    if 'B' in flags:
+        output_data['structures'][0]['bountyType'] = "PKO"
+        output_data['structures'][0]['progressiveFactor'] = 0.5
+
+    tournament_entries = data_dict['CompletedTournament'].get('TournamentEntry', [])
+    prize_dict = {}
+
+    for entry in tournament_entries:
+        position = entry['@position']
+        prize = float(entry.get('@prize', 0))
+        prize_bounty_component = float(entry.get('@prizeBountyComponent', 0))
+        calculated_prize = prize - prize_bounty_component
+        calculated_prize = round(calculated_prize, 2)
+
+        if calculated_prize > 0:
+            prize_dict[position] = calculated_prize
+
+    output_data['structures'][0]['prizes'] = prize_dict
+
+    output_file_path = os.path.join("output", "output.json")
+    os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+    with open(output_file_path, 'w') as file:
+        json.dump(output_data, file, indent=2)
+
+    return send_file(output_file_path, as_attachment=True, download_name='output.json')
+
+@app.route('/process_images', methods=['POST'])
 def process_images():
     # Diretório temporário para armazenar as imagens enviadas
     input_dir = "temp_images"
@@ -318,5 +393,4 @@ def read_xml(xml_file):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
 

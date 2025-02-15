ad89af36a5c2973fb9d52a9fb36443ddbacd1c50
1rforti <ricardoforti@hotmail.com>
2025-02-15 02:48:44 -0300

    main2

--- main2.py
+++ main2.py
@@ -18,6 +18,63 @@ def home():
 
 @app.route('/generate_json', methods=['POST'])
 def generate_json():
+    xml_file = request.files.get('xml_file')
+    stack_inicial = request.form.get('stack_inicial')
+
+    if not stack_inicial:
+        return jsonify({"error": "Por favor, informe o stack inicial."}), 400
+
+    if not xml_file:
+        return jsonify({"error": "Por favor, selecione um arquivo XML."}), 400
+
+    data_dict = read_xml(xml_file)
+    tournament_info = data_dict['CompletedTournament']
+    tournament_name = tournament_info['@name']
+    total_entrants = int(tournament_info['@totalEntrants']) + int(tournament_info['@reEntries'])
+    flags = tournament_info['@flags']
+
+    chips = total_entrants * int(stack_inicial)
+
+    output_data = {
+        "name": "/",
+        "folders": [],
+        "structures": [
+            {
+                "name": tournament_name,
+                "chips": chips,
+                "prizes": {}
+            }
+        ]
+    }
+
+    if 'B' in flags:
+        output_data['structures'][0]['bountyType'] = "PKO"
+        output_data['structures'][0]['progressiveFactor'] = 0.5
+
+    tournament_entries = data_dict['CompletedTournament'].get('TournamentEntry', [])
+    prize_dict = {}
+
+    for entry in tournament_entries:
+        position = entry['@position']
+        prize = float(entry.get('@prize', 0))
+        prize_bounty_component = float(entry.get('@prizeBountyComponent', 0))
+        calculated_prize = prize - prize_bounty_component
+        calculated_prize = round(calculated_prize, 2)
+
+        if calculated_prize > 0:
+            prize_dict[position] = calculated_prize
+
+    output_data['structures'][0]['prizes'] = prize_dict
+
+    output_file_path = os.path.join("output", "output.json")
+    os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+    with open(output_file_path, 'w') as file:
+        json.dump(output_data, file, indent=2)
+
+    return send_file(output_file_path, as_attachment=True, download_name='output.json')
+
+@app.route('/process_images', methods=['POST'])
+def process_images():
     # Diretório temporário para armazenar as imagens enviadas
     input_dir = "temp_images"
     os.makedirs(input_dir, exist_ok=True)
@@ -260,7 +317,7 @@ def generate_json():
         tournament_name = tournament_name_lines[0]
 
     # Recupere o valor do stack inicial do formulário
-    stack_inicial = request.form.get('stack_inicial')
+    stack_inicial = request.form.get('stack_inicial_images')
     if stack_inicial is None:
         return jsonify({"error": "Por favor, informe o valor do stack inicial."}), 400
 
@@ -330,5 +387,10 @@ def generate_json():
 
     return send_file(output_file_path, as_attachment=True, download_name='output.json')
 
+def read_xml(xml_file):
+    data_dict = xmltodict.parse(xml_file.read())
+    return data_dict
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+

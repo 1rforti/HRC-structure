e706889f6a8607029eb48f097a0b0234dc5877d6
1rforti <ricardoforti@hotmail.com>
2025-02-17 06:03:50 -0300

    r

--- main2.py
+++ main2.py
@@ -392,5 +392,6 @@ def read_xml(xml_file):
     return data_dict
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port)
 
